39b70c2f560d80dcd977d84b69cb96220260a954
Gentrit <gentritrexhaa@gmail.com>
2023-03-26 17:18:29 +0200

    v2.6 WL remove button is added

--- assets/watch-later.js
+++ assets/watch-later.js
@@ -12,12 +12,15 @@ footer.innerHTML = Footer();
 
 const content = document.getElementById('wl_container');
 
+wlFunction();
+
+function wlFunction() {
 const wl_array = JSON.parse(localStorage.getItem('wl_array'));
-console.log(wl_array);
+// console.log(wl_array);
 
 wl_array.forEach(e => {
     content.innerHTML += wlContent();
-    console.log(e);
+    // console.log(e);
 });
 
 const wl_img = document.getElementsByClassName('wl_img');
@@ -32,18 +35,33 @@ for(let i=0; i<wl_array.length; i++){
     wl_title[i].innerHTML = wl_array[i].name;
     wl_genre[i].innerHTML = wl_array[i].genres;
     wl_year[i].innerHTML = wl_array[i].premiered.slice(0,4);
+    wl_remove[i].setAttribute('data-id',wl_array[i].externals.thetvdb);
     wl_details[i].addEventListener('click', ()=>{
         const card_id = wl_array[i].externals.thetvdb;
         const url = `show.html?id=${card_id}`;
         window.open(url,'SHOW')
     })
+    wl_remove[i].addEventListener('click',(e)=>{
+        let wl_id = e.target.getAttribute('data-id');
+        console.log(`Remoev button vas clicked:   ${wl_id}`);
+        let index=0; 
+        wl_array.filter((elem) => { 
+            if(elem.externals.thetvdb == wl_id){
+                console.log(`index :  ${index}`);
+                wl_array.splice(index, 1);
+                localStorage.setItem('wl_array',JSON.stringify(wl_array));
+                location.reload();
+            }
+
+            // console.log(`Bolean result:  ${elem.externals.thetvdb}`);
+            // console.log(`id prej filterit 2: ${elem.externals.thetvdb}`);}
+        ;
+        })
+    })
+}
 }
 
-// const content = document.querySelector('.container');
-
-// // content.innerHTML = "HELLO";
-// // content.innerHTML = ShowContent();
-
+//when we search it redicts us to main page and makes a search
 const button = document.querySelector('#searchButton');
 button.addEventListener('click', (e) => {
   e.preventDefault();
